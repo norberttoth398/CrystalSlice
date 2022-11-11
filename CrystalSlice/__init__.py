41711f33af584b97c25ff9b7e5fa338ecc96a1df
Norbert Toth <norbert19982@gmail.com>
2022-11-11 13:40:56 +0000

    plagioclase removed

--- CrystalSlice/__init__.py
+++ CrystalSlice/__init__.py
@@ -1,6 +1,5 @@
 from .Cuboid import Cuboid
 from .WulffCrystal import WulffCrystal
-from .Plagioclase import PlagCrystal, ProportionalPlagCrystal
 from .Zoned import ZonedCuboid, ZonedPlag
 from .ellipsoid import Ellipsoid
 from .custom_object import Custom
