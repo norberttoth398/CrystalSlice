655c07dae9ddf2cbe1dbd0b7cf5b4ba344f4e67e
Norbert Toth <norbert19982@gmail.com>
2022-11-11 13:35:38 +0000

    update for custom object

--- CrystalSlice/__init__.py
+++ CrystalSlice/__init__.py
@@ -3,4 +3,5 @@ from .WulffCrystal import WulffCrystal
 from .Plagioclase import PlagCrystal, ProportionalPlagCrystal
 from .Zoned import ZonedCuboid, ZonedPlag
 from .ellipsoid import Ellipsoid
+from .custom_object import Custom
 
